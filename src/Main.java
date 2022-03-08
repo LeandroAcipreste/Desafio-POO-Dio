c8caebe249a630d0180906ad2c02aa4f4594042c
LeandroAcipreste <logacipreste@gmail.com>
2022-03-08 12:28:35 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,40 @@ public class Main {
         mentoria.setDescricao("descriçao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /**System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);**/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos:" + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos:" + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Conluídos:" + devCamila.get);
+        System.out.println("XP:" + devCamila.calcularTotalXp());
+
+        System.out.println("---------------");
+
+        Dev devLeandro = new Dev();
+        devLeandro.setNome("Leandro");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos:" + devLeandro.getConteudosInscritos());
+        devLeandro.progredir();
+        devLeandro.progredir();
+        devLeandro.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos:" + devLeandro.getConteudosInscritos());
+        System.out.println("Conteúdos Conluídos:" + devLeandro.getConteudosConcluidos());
+        System.out.println("XP:" + devLeandro.calcularTotalXp());
     }
 }
---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,71 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsinscritos().add(this);
+    }
+
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+            System.err.println("Você não está matriculado em nenhuma conteúdo");
+        }
+    }
+
+    public double calcularTotalXp(){
+        return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> conteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setconteudosConcluidos(Set<Conteudo> conteudoConcluidos) {
+        this.conteudosConcluidos = conteudoConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudoConcluidos, dev.conteudoConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+
+    public String conteudosConcluidos() {
+    }
+}
